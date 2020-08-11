52f8f830a0f6b7d9e71a2503a81a558a9241d174
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-08-11 20:38:53 +0530

    add background.js

--- popup.js
+++ popup.js
@@ -1,49 +1,62 @@
-console.log("popus js ready!");
-Boolean PLAY = true;
-document.getElementById("reset-btn").addEventListener("click", resetTimer);
-function resetTimer() {
-    document.getElementById("timer-min").innerHTML = '20';
-    document.getElementById("timer-sec").innerHTML = '00';
-}
 
-document.getElementById("play-pause-btn").addEventListener("click", () => {
-
-	if(i % 2 == 0){
-		i+=1;
-		playtime();
-	}else {
-		i = 0;
-		stoptime();
-	}
-
-    const countdownMin = document.getElementById("timer-min");
-    const countdownSec = document.getElementById("timer-sec");
-
-	const startingMin = document.getElementById("screen-time").value;
-	let time = 1 * 60;
-	console.log(time);
-
-	while( time >= 0){
-		setInterval(updateCountdown, 1000);
-	}
+var setIntervalVar;
+document.getElementById("reset-btn").addEventListener("click", () =>{
+    document.getElementById("screen-timer-min").innerHTML = '20';
+	document.getElementById("screen-timer-sec").innerHTML = '00';
 	
-	
-	
+	document.getElementById("break-timer-sec").innerHTML = '20';
 
-    function updateCountdown() {
-        const mintues = Math.floor(time / 60);
-        let seconds = time % 60;
-		seconds = seconds < 10 ? '0'+seconds : seconds;
-        countdownMin.innerHTML = mintues;
-        countdownSec.innerHTML = seconds;
-        time--;
-    }
 });
 
-function playtime(){
 
-}
+chrome.storage.sync.get(['screen_time', 'break_time', 'show_notification', 'play_sound'], function(obj){
+	
+	if(obj.screen_time){
+		document.getElementById("screen-timer-min").innerHTML = (obj.screen_time).toString();	
+	}
 
-function stoptime(){
+	if(obj.break_time){
+		document.getElementById("break-timer-sec").innerHTML = (obj.break_time).toString();
+	}
 
-}
+	if(obj.show_notification){
+		document.getElementById("show-notification").checked = true;
+	}
+
+	if(obj.play_sound){
+		document.getElementById("play-sound").checked = true;
+	}
+
+});
+
+
+document.getElementById("screen-time").addEventListener("change", ()=>{
+	var getScreenTime = document.getElementById("screen-time").value;
+	document.getElementById("screen-timer-min").innerHTML = getScreenTime;
+	chrome.storage.sync.set({'screen_time': parseInt(getScreenTime)});
+	
+});
+
+document.getElementById("break-time").addEventListener("change", ()=>{
+	var getBreakTime = document.getElementById("break-time").value;
+	document.getElementById("break-timer-sec").innerHTML = getBreakTime; 
+	chrome.storage.sync.set({'break_time': parseInt(getBreakTime)});
+});
+
+document.getElementById("show-notification").addEventListener("change", ()=>{
+	var show_notification_checkbox = document.getElementById("show-notification");
+	if(show_notification_checkbox.checked){
+		chrome.storage.sync.set({'show_notification':true});
+	}else{
+		chrome.storage.sync.set({'show_notification':false});
+	}
+});
+
+document.getElementById("play-sound").addEventListener("change", ()=>{
+	var play_sound_checkbox = document.getElementById("play-sound");
+	if(play_sound_checkbox.checked){
+		chrome.storage.sync.set({'play_sound':true});
+	}else{
+		chrome.storage.sync.set({'play_sound':false});
+	}
+});
