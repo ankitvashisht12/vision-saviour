cbbff9a4f59cfd1df9ca082eac04d0fd3a6acf35
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-08-12 01:18:33 +0530

    working correctly with notificaition

---
+++ background.js
@@ -0,0 +1,67 @@
+// Counter Logic
+var screenInterval, breakInterval;
+chrome.storage.sync.get(['start_screen_time', 'start_break_time', 'screen_time', 'break_time', 'show_notification', 'play_sound'], function(res){
+	startScreenCountDown(res);
+});
+
+function startScreenCountDown(res){
+
+	const startingMin = res.start_screen_time;
+	let time = 1 * 60; // change time here;
+
+	screenInterval = setInterval(() => {
+		
+		if(time == 0){
+			chrome.notifications.create("Break Start Notification", {
+				type: "basic",
+				iconUrl: "icon_128.png",
+				title: "Break Time Starts",
+				"message": "Take atleast 20 seconds break. See 20 feet distant."
+
+			});
+			clearInterval(screenInterval);
+			chrome.runtime.sendMessage({screen_min : startingMin, screen_sec : '00'});
+			startBreakCountdown(res);
+		}
+		
+		const mintues = Math.floor(time / 60);
+		let seconds = time % 60;
+		seconds = seconds < 10 ? '0'+seconds : seconds;
+
+		chrome.storage.sync.set({'screen_time': parseInt(mintues), 'screen_time_sec': parseInt(seconds)});
+		console.log("MINUTE "+mintues+" SECONDS "+seconds);
+
+		chrome.runtime.sendMessage({screen_min : mintues.toString(), screen_sec : seconds.toString()});
+		time--;
+	}, 1000);
+}
+
+
+function startBreakCountdown(res){
+	
+	let breakTime = res.start_break_time;
+	const startBreakTime = res.start_break_time;
+	
+	breakInterval = setInterval( ()=>{
+		if(breakTime == 0){
+			chrome.notifications.create("Break End Notification", {
+				type: "basic",
+				iconUrl: "icon_128.png",
+				title: "Screen Time Starts",
+				"message": "You can resume your work now."
+
+			});
+			clearInterval(breakInterval);
+			chrome.runtime.sendMessage({break_sec : startBreakTime});
+			startScreenCountDown(res);
+		}
+		let seconds = breakTime % 60;
+		seconds = seconds < 10 ? '0'+seconds : seconds;
+		chrome.storage.sync.set({'break_time': parseInt(seconds)});
+		console.log("BREAK TIME : 00:"+seconds);
+	
+		chrome.runtime.sendMessage({break_sec : seconds.toString()});
+		breakTime--;
+
+	}, 1000);
+}
--- popup.js
+++ popup.js
@@ -1,22 +1,27 @@
 
-var setIntervalVar;
-document.getElementById("reset-btn").addEventListener("click", () =>{
+document.getElementById("reset-btn").addEventListener("click", reset);
+
+function reset(){
     document.getElementById("screen-timer-min").innerHTML = '20';
 	document.getElementById("screen-timer-sec").innerHTML = '00';
-	
 	document.getElementById("break-timer-sec").innerHTML = '20';
 
-});
+	chrome.storage.sync.set({'start_screen_time' : 20, 'start_break_time': 20, 'screen_time': 20, 'screen_time_sec': 0, 'break_time': 20, 'show_notification': true, 'play_sound': true});
+	
+};
 
-
-chrome.storage.sync.get(['screen_time', 'break_time', 'show_notification', 'play_sound'], function(obj){
+chrome.storage.sync.get(['screen_time', 'screen_time_sec', 'break_time', 'show_notification', 'play_sound'], function(obj){
 	
 	if(obj.screen_time){
-		document.getElementById("screen-timer-min").innerHTML = (obj.screen_time).toString();	
+		document.getElementById("screen-timer-min").innerHTML = obj.screen_time	
+	}
+
+	if(obj.screen_time_sec){
+		document.getElementById("screen-timer-sec").innerHTML = obj.screen_time_sec
 	}
 
 	if(obj.break_time){
-		document.getElementById("break-timer-sec").innerHTML = (obj.break_time).toString();
+		document.getElementById("break-timer-sec").innerHTML = obj.break_time
 	}
 
 	if(obj.show_notification){
@@ -30,10 +35,23 @@ chrome.storage.sync.get(['screen_time', 'break_time', 'show_notification', 'play
 });
 
 
+chrome.runtime.onMessage.addListener(
+	function(obj, sender, sendResponse) {
+		if(obj.screen_min){
+			document.getElementById("screen-timer-min").innerHTML = obj.screen_min;
+			document.getElementById("screen-timer-sec").innerHTML = obj.screen_sec;
+		}else{
+			document.getElementById("break-timer-sec").innerHTML = obj.break_sec;
+		}
+});
+
+
 document.getElementById("screen-time").addEventListener("change", ()=>{
 	var getScreenTime = document.getElementById("screen-time").value;
 	document.getElementById("screen-timer-min").innerHTML = getScreenTime;
 	chrome.storage.sync.set({'screen_time': parseInt(getScreenTime)});
+	chrome.storage.sync.set({'start_screen_time': parseInt(getScreenTime)});
+
 	
 });
 
@@ -41,6 +59,8 @@ document.getElementById("break-time").addEventListener("change", ()=>{
 	var getBreakTime = document.getElementById("break-time").value;
 	document.getElementById("break-timer-sec").innerHTML = getBreakTime; 
 	chrome.storage.sync.set({'break_time': parseInt(getBreakTime)});
+	chrome.storage.sync.set({'start_break_time': parseInt(getBreakTime)});
+
 });
 
 document.getElementById("show-notification").addEventListener("change", ()=>{
@@ -60,3 +80,5 @@ document.getElementById("play-sound").addEventListener("change", ()=>{
 		chrome.storage.sync.set({'play_sound':false});
 	}
 });
+
+chrome.windows.onRemoved.addListener(reset);
